494ffbb934934aa9065fbc6a6112d2c316223f24
Mackelf <mac.maxwell@gmail.com>
2026-06-11 15:12:46 -0400

    style: format archetype mappings for better readability

--- JS/archetypeMap.js
+++ JS/archetypeMap.js
@@ -1,90 +1,182 @@
 export const ARCHETYPE_CARD_MAP = {
   // MODERN
   "MOD-Amulet Titan": { main: "Amulet of Vigor", key: "Primeval Titan" },
-  "MOD-Azorius Blink": { main: "Phelia, Exuberant Shepherd", key: "Quantum Riddler" },
+  "MOD-Azorius Blink": {
+    main: "Phelia, Exuberant Shepherd",
+    key: "Quantum Riddler",
+  },
   "MOD-Azorius Control": { main: "Teferi, Hero of Dominaria", key: "Solitude" },
   "MOD-Azorius Scepter": { main: "Isochron Scepter", key: "Orim's Chant" },
   "MOD-Belcher": { main: "Goblin Charbelcher", key: "Lotus Bloom" },
-  "MOD-Boros Belcher": { main: "Goblin Charbelcher", key: "Leyline of Resonance" },
+  "MOD-Boros Belcher": {
+    main: "Goblin Charbelcher",
+    key: "Leyline of Resonance",
+  },
   "MOD-Boros Burn": { main: "Boros Charm", key: "Eidolon of the Great Revel" },
   "MOD-Boros Energy": { main: "Guide of Souls", key: "Ajani, Nacatl Pariah" },
   "MOD-Boros LD": { main: "Price of Freedom", key: "Cleansing Wildfire" },
   "MOD-Boros Hollow-vine": { main: "Hollow One", key: "Vengevine" },
-  "MOD-Dimir Midrange": { main: "Psychic Frog", key: "Kaito, Bane of Nightmares" },
+  "MOD-Dimir Midrange": {
+    main: "Psychic Frog",
+    key: "Kaito, Bane of Nightmares",
+  },
   "MOD-Dimir Mill": { main: "Hedron Crab", key: "Tasha's Hideous Laughter" },
   "MOD-Dimir Oculus": { main: "Abhorrent Oculus", key: "Unearth" },
   "MOD-Dimir Sorin": { main: "Sorin, Imperious Bloodlord", key: "Vein Ripper" },
   "MOD-Domain Zoo": { main: "Leyline Binding", key: "Territorial Kavu" },
-  "MOD-Eldrazi Ramp": { main: "Sowing Mycospawn", key: "Ulamog, the Infinite Gyre" },
-  "MOD-Eldrazi Tron": { main: "Thought-Knot Seer", key: "Karn, the Great Creator" },
-  "MOD-Esper Blink": { main: "Phelia, Exuberant Shepherd", key: "Overlord of the Balemurk" },
-  "MOD-Esper Control": { main: "Psychic Frog", key: "Teferi, Hero of Dominaria" },
-  "MOD-Esper Midrange": { main: "Psychic Frog", key: "Teferi, Hero of Dominaria" },
+  "MOD-Eldrazi Ramp": {
+    main: "Sowing Mycospawn",
+    key: "Ulamog, the Infinite Gyre",
+  },
+  "MOD-Eldrazi Tron": {
+    main: "Thought-Knot Seer",
+    key: "Karn, the Great Creator",
+  },
+  "MOD-Esper Blink": {
+    main: "Phelia, Exuberant Shepherd",
+    key: "Overlord of the Balemurk",
+  },
+  "MOD-Esper Control": {
+    main: "Psychic Frog",
+    key: "Teferi, Hero of Dominaria",
+  },
+  "MOD-Esper Midrange": {
+    main: "Psychic Frog",
+    key: "Teferi, Hero of Dominaria",
+  },
   "MOD-Golgari Elves": { main: "Shaman of the Pack", key: "Collected Company" },
   "MOD-Goryo's Vengeance": { main: "Goryo's Vengeance", key: "Griselbrand" },
   "MOD-Grixis Phoenix": { main: "Arclight Phoenix", key: "Preordain" },
   "MOD-Grixis Reanimator": { main: "Archon of Cruelty", key: "Persist" },
-  "MOD-Gruul Basking Broodscale Combo": { main: "Basking Broodscale", key: "Blade of the Bloodchief" },
-  "MOD-Gruul Valakut": { main: "Scapeshift", key: "Valakut, the Molten Pinnacle" },
+  "MOD-Gruul Basking Broodscale Combo": {
+    main: "Basking Broodscale",
+    key: "Blade of the Bloodchief",
+  },
+  "MOD-Gruul Valakut": {
+    main: "Scapeshift",
+    key: "Valakut, the Molten Pinnacle",
+  },
   "MOD-Hammer Time": { main: "Colossus Hammer", key: "Puresteel Paladin" },
   "MOD-Izzet Affinity": { main: "Kappa Cannoneer", key: "Pinnacle Emissary" },
   "MOD-Izzet Prowess": { main: "Slickshot Show-Off", key: "Murmuring Mystic" },
-  "MOD-Jeskai Blink": { main: "Phelia, Exuberant Shepherd", key: "Teferi, Time Raveler" },
+  "MOD-Jeskai Blink": {
+    main: "Phelia, Exuberant Shepherd",
+    key: "Teferi, Time Raveler",
+  },
   "MOD-Jeskai Energy": { main: "Wrath of the Skies", key: "Ocelot Pride" },
   "MOD-Jundfect": { main: "Venerated Rotpriest", key: "Scale Up" },
   "MOD-Jund Hollow-vine": { main: "Hollow One", key: "Vengevine" },
   "MOD-Living End": { main: "Living End", key: "Violent Outburst" },
   "MOD-Merfolk": { main: "Lord of Atlantis", key: "Svyelun of Sea and Sky" },
-  "MOD-Mono Black Midrange": { main: "Necropotence", key: "Sheoldred, the Apocalypse" },
-  "MOD-Mono Blue Belcher": { main: "Goblin Charbelcher", key: "Tameshi, Reality Architect" },
-  "MOD-Mono Green Basking Broodscale Combo": { main: "Basking Broodscale", key: "Blade of the Bloodchief" },
-  "MOD-Mono Red Belcher": { main: "Goblin Charbelcher", key: "Stormscale Scion" },
+  "MOD-Mono Black Midrange": {
+    main: "Necropotence",
+    key: "Sheoldred, the Apocalypse",
+  },
+  "MOD-Mono Blue Belcher": {
+    main: "Goblin Charbelcher",
+    key: "Tameshi, Reality Architect",
+  },
+  "MOD-Mono Green Basking Broodscale Combo": {
+    main: "Basking Broodscale",
+    key: "Blade of the Bloodchief",
+  },
+  "MOD-Mono Red Belcher": {
+    main: "Goblin Charbelcher",
+    key: "Stormscale Scion",
+  },
   "MOD-Mono Red Burn": { main: "Lightning Bolt", key: "Boltwave" },
   "MOD-Mono White Control": { main: "Solitude", key: "The Wandering Emperor" },
   "MOD-Neobrand": { main: "Neoform", key: "Griselbrand" },
   "MOD-Rakdos Hollow": { main: "Hollow One", key: "Monument to Endurance" },
-  "MOD-Rakdos Midrange": { main: "Kroxa, Titan of Death's Hunger", key: "Sheoldred, the Apocalypse" },
-  "MOD-Rhino Domain": { main: "Crashing Footfalls", key: "Leyline of the Guildpact" },
+  "MOD-Rakdos Midrange": {
+    main: "Kroxa, Titan of Death's Hunger",
+    key: "Sheoldred, the Apocalypse",
+  },
+  "MOD-Rhino Domain": {
+    main: "Crashing Footfalls",
+    key: "Leyline of the Guildpact",
+  },
   "MOD-Ruby Storm": { main: "Ral, Monsoon Mage", key: "Ruby Medallion" },
-  "MOD-Samwise Combo": { main: "Samwise Gamgee", key: "Rosie Cotton of South Lane" },
-  "MOD-Song of Creation": { main: "Song of Creation", key: "Jace, Wielder of Mysteries" },
+  "MOD-Samwise Combo": {
+    main: "Samwise Gamgee",
+    key: "Rosie Cotton of South Lane",
+  },
+  "MOD-Song of Creation": {
+    main: "Song of Creation",
+    key: "Jace, Wielder of Mysteries",
+  },
   "MOD-Sultai Midrange": { main: "Psychic Frog", key: "Tarmogoyf" },
-  "MOD-Temur Prowess": { main: "Cori-Steel Cutter", key: "Monastery Swiftspear" },
+  "MOD-Temur Prowess": {
+    main: "Cori-Steel Cutter",
+    key: "Monastery Swiftspear",
+  },
   "MOD-Temur Cascade": { main: "Crashing Footfalls", key: "Violent Outburst" },
-  "MOD-Yawgmoth": { main: "Yawgmoth, Thran Physician", key: "Strangleroot Geist" },
-  "MOD-Zoo Reanimator": { main: "Leyline Binding", key: "Atraxa, Grand Unifier" },
-
+  "MOD-Yawgmoth": {
+    main: "Yawgmoth, Thran Physician",
+    key: "Strangleroot Geist",
+  },
+  "MOD-Zoo Reanimator": {
+    main: "Leyline Binding",
+    key: "Atraxa, Grand Unifier",
+  },
 
   // STANDARD
   "STD-4c Control": { main: "Jeskai Revelation", key: "Atraxa, Grand Unifier" },
-  "STD-5c Elementals": { main: "Sunderflock", key: "Omnath, Locus of Creation" },
-  "STD-Azorius Tempo": { main: "Aang, Swift Savior", key: "Loran of the Third Path" },
-  "STD-Bant Tortuga": { main: "Smuggler's Surprise", key: "Overlord of the Haemtusk" },
+  "STD-5c Elementals": {
+    main: "Sunderflock",
+    key: "Omnath, Locus of Creation",
+  },
+  "STD-Azorius Tempo": {
+    main: "Aang, Swift Savior",
+    key: "Loran of the Third Path",
+  },
+  "STD-Bant Tortuga": {
+    main: "Smuggler's Surprise",
+    key: "Overlord of the Haemtusk",
+  },
   "STD-Boros Burn": { main: "Boros Charm", key: "Eidolon of the Great Revel" },
-  "STD-Boros Dragons": { main: "Sarkhan, Dragon Ascendant", key: "Tyrrant's Scorn" },
-  "STD-Dimir Excruciator": { main: "Doomsday Excruciator", key: "Psychic Frog" },
-  "STD-Izzet Prowess": { main: "Stormchaser's Talent", key: "Slickshot Show-Off" },
+  "STD-Boros Dragons": {
+    main: "Sarkhan, Dragon Ascendant",
+    key: "Tyrrant's Scorn",
+  },
+  "STD-Dimir Excruciator": {
+    main: "Doomsday Excruciator",
+    key: "Psychic Frog",
+  },
+  "STD-Izzet Prowess": {
+    main: "Stormchaser's Talent",
+    key: "Slickshot Show-Off",
+  },
   "STD-Izzet Spellementals": { main: "Eddymurk Crab", key: "Risen Wetlands" },
   "STD-Jeskai Control": { main: "Jeskai Revelation", key: "Sunfall" },
   "STD-Mardu Discard": { main: "Marauding Mako", key: "Orcish Bowmasters" },
-  "STD-Mono Green Landfall": { main: "Sazh's Chocobo", key: "Ashaya, Soul of the Wild" },
+  "STD-Mono Green Landfall": {
+    main: "Sazh's Chocobo",
+    key: "Ashaya, Soul of the Wild",
+  },
   "STD-Mono Red Aggro": { main: "Nova Hellkite", key: "Witchstalker Frenzy" },
   "STD-MonoRedU": { main: "Scalding Viper", key: "Slickshot Show-Off" },
-  "STD-Naya Yuna": { main: "Yuna, Hope of Spira", key: "Jetmir, Nexus of Revels" },
-  "STD-Orzhov Ketramose": { main: "Ketramose, the New Dawn", key: "Sheoldred, the Apocalypse" },
-  "STD-Selesnya Landfall": { main: "Dyadrine, Synthesis Amalgam", key: "Ashaya, Soul of the Wild" },
+  "STD-Naya Yuna": {
+    main: "Yuna, Hope of Spira",
+    key: "Jetmir, Nexus of Revels",
+  },
+  "STD-Orzhov Ketramose": {
+    main: "Ketramose, the New Dawn",
+    key: "Sheoldred, the Apocalypse",
+  },
+  "STD-Selesnya Landfall": {
+    main: "Dyadrine, Synthesis Amalgam",
+    key: "Ashaya, Soul of the Wild",
+  },
   "STD-Selesnya Ouroboroid": { main: "Ouroboroid", key: "Deeproot Champion" },
 
-
   // PreModern
-  "PDM-Burn": { main: "lightning bolt", key: "Black Vise" },
-  "PDM-Contamination": { main: "Contamination", key: "Zombie Infestation" },
+  "PMD-Burn": { main: "lightning bolt", key: "Black Vise" },
+  "PMD-Contamination": { main: "Contamination", key: "Zombie Infestation" },
   "PMD-Hermit FEB": { main: "Hermit Druid", key: "Volrath's Shapeshifter" },
   "PMD-Oath Ponza": { main: "Oath of Druids", key: "Terravore" },
   "PMD-Stasis": { main: "stasis", key: "Root Maze" },
-  "PDM-Stiflenought": { main: "Phyrexian Dreadnought", key: "Stifle" },
-  
-  
+  "PMD-Stiflenought": { main: "Phyrexian Dreadnought", key: "Stifle" },
 };
 
 export async function getScryfallImage(cardName) {
@@ -100,9 +192,9 @@ export async function getScryfallImage(cardName) {
       `https://api.scryfall.com/cards/named?fuzzy=${encodeURIComponent(cardName)}&format=json`,
       {
         headers: {
-          "Accept": "application/json"
-        }
-      }
+          Accept: "application/json",
+        },
+      },
     );
     if (!res.ok) {
       console.warn(`[Scryfall] ${res.status} para "${cardName}"`);
@@ -112,7 +204,10 @@ export async function getScryfallImage(cardName) {
     const uris = data.image_uris ?? data.card_faces?.[0]?.image_uris ?? null;
     console.log(`[Scryfall] "${cardName}" uris:`, uris); // ← LOG
     if (!uris) return null;
-    const result = { art_crop: uris.art_crop ?? null, normal: uris.normal ?? null };
+    const result = {
+      art_crop: uris.art_crop ?? null,
+      normal: uris.normal ?? null,
+    };
     console.log(`[Scryfall] result:`, result); // ← LOG
     localStorage.setItem(cacheKey, JSON.stringify(result));
     return result;
